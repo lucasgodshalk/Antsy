f86b21a2049e321e99a1b93473c9283b21e4d620
Lucas Godshalk <lucasmg91@gmail.com>
2017-01-07 20:54:32 -0500

    Shell of library

---
+++ Antsy/AntsyHost.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antsy
+{
+    /// <summary>
+    /// For those who want to get started.
+    /// </summary>
+    public class AntsyHost
+    {
+        private IWebHostBuilder _builder;
+
+        public AntsyHost(int port)
+        {
+            _builder = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://*:{port}")
+                .ConfigureServices(ConfigureServices)
+                .Configure(Configure);
+        }
+
+        public void Run()
+        {
+            var host = _builder.Build();
+            host.Run();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        private void Configure(IApplicationBuilder app)
+        {
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("path", new RequestDelegate(context => Task.FromResult(0)));
+            app.UseRouter(routeBuilder.Build());
+        }
+    }
+}
---
+++ Simple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Antsy;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AntsyHost server = new AntsyHost(8000);
+        // /hello
+        //server.GetJson("hello", (req, res) => 
+        //{
+        //    //A regular object gets serialized for you.
+        //    return new
+        //    {
+        //        title = "boss",
+        //        name = "jenny"
+        //    };
+        //});
+
+        //// /goodbye
+        //server.GetJson("goodbye", (req, res) =>
+        //{
+        //    //Or send your own json string.
+        //    return "{title: \"boss\", name: \"jenny\"}";
+        //});
+        server.Run();
+    }
+}
