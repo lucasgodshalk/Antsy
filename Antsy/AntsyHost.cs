61f98f22953c946b65253e8bbc630be6f8b15ccc
Lucas Godshalk <lucasmg91@gmail.com>
2017-01-07 21:24:58 -0500

    Filled in basics of get, post, and delete

--- Antsy/AntsyHost.cs
+++ Antsy/AntsyHost.cs
@@ -17,6 +17,10 @@ namespace Antsy
     /// </summary>
     public class AntsyHost
     {
+        private List<Tuple<string, Func<AntsyRequest, AntsyResponse, Task>>> getList = new List<Tuple<string, Func<AntsyRequest, AntsyResponse, Task>>>();
+        private List<Tuple<string, Func<AntsyRequest, AntsyResponse, Task>>> postList = new List<Tuple<string, Func<AntsyRequest, AntsyResponse, Task>>>();
+        private List<Tuple<string, Func<AntsyRequest, AntsyResponse, Task>>> deleteList = new List<Tuple<string, Func<AntsyRequest, AntsyResponse, Task>>>();
+
         private IWebHostBuilder _builder;
 
         public AntsyHost(int port)
@@ -33,7 +37,19 @@ namespace Antsy
         /// </summary>
         public void Get(string path, Action<AntsyRequest, AntsyResponse> del)
         {
+            Get(path, (req, res) =>
+            {
+                del(req, res);
+                return Task.FromResult(0);
+            });
+        }
 
+        /// <summary>
+        /// Handle a GET request at the given path.
+        /// </summary>
+        public void Get(string path, Func<AntsyRequest, AntsyResponse, Task> del)
+        {
+            getList.Add(Tuple.Create(path, del));
         }
 
         /// <summary>
@@ -41,7 +57,19 @@ namespace Antsy
         /// </summary>
         public void Post(string path, Action<AntsyRequest, AntsyResponse> del)
         {
+            Post(path, (req, res) =>
+            {
+                del(req, res);
+                return Task.FromResult(0);
+            });
+        }
 
+        /// <summary>
+        /// Handle a POST request at the given path.
+        /// </summary>
+        public void Post(string path, Func<AntsyRequest, AntsyResponse, Task> del)
+        {
+            postList.Add(Tuple.Create(path, del));
         }
 
         /// <summary>
@@ -49,7 +77,19 @@ namespace Antsy
         /// </summary>
         public void Delete(string path, Action<AntsyRequest, AntsyResponse> del)
         {
+            Delete(path, (req, res) =>
+            {
+                del(req, res);
+                return Task.FromResult(0);
+            });
+        }
 
+        /// <summary>
+        /// Handle a DELETE request at the given path.
+        /// </summary>
+        public void Delete(string path, Func<AntsyRequest, AntsyResponse, Task> del)
+        {
+            deleteList.Add(Tuple.Create(path, del));
         }
 
         public void Run()
@@ -66,6 +106,27 @@ namespace Antsy
         private void Configure(IApplicationBuilder app)
         {
             var routeBuilder = new RouteBuilder(app);
+            foreach (var item in getList)
+            {
+                routeBuilder.MapGet(item.Item1, new RequestDelegate(context => 
+                {
+                    return item.Item2(new AntsyRequest(context.Request), new AntsyResponse(context.Response));
+                }));
+            }
+            foreach (var item in postList)
+            {
+                routeBuilder.MapPost(item.Item1, new RequestDelegate(context =>
+                {
+                    return item.Item2(new AntsyRequest(context.Request), new AntsyResponse(context.Response));
+                }));
+            }
+            foreach (var item in deleteList)
+            {
+                routeBuilder.MapDelete(item.Item1, new RequestDelegate(context =>
+                {
+                    return item.Item2(new AntsyRequest(context.Request), new AntsyResponse(context.Response));
+                }));
+            }
             routeBuilder.MapGet("path", new RequestDelegate(context => Task.FromResult(0)));
             app.UseRouter(routeBuilder.Build());
         }
