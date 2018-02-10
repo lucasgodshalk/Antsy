8bb6a07b5294e981c67e36d76338e3b2489c080f
Lucas Godshalk <lucasmg91@gmail.com>
2018-02-10 11:17:14 -0700

    Bump version

--- release.bat
+++ release.bat
@@ -1,3 +1,1 @@
-dotnet pack -c Release --output nupkgs
-
-nuget push nupkgs/antsy.<version>.nupkg -source https://www.nuget.org/api/v2/package
+nuget push src/nupkgs/antsy.<version>.nupkg -source https://www.nuget.org/api/v2/package
